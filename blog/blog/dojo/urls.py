a21222eafb99f693b4360920ca61729268350d9d
fullyalive <wogud1202@gmail.com>
2018-07-15 17:51:41 +0900

    4 patterns of Function Based View

--- blog/blog/dojo/urls.py
+++ blog/blog/dojo/urls.py
@@ -8,4 +8,8 @@ app_name = "dojo"
 urlpatterns = [
     path("sum/<numbers>/", views.mysum, name="mysum"),
     path("hello/<name>/<age>", views.hello, name="hello"),
+    path("list1/", views.post_list1, name="post_list1"),
+    path("list2/", views.post_list2, name="post_list2"),
+    path("list3/", views.post_list3, name="post_list3"),
+    path("excel/", views.excel_download, name="excel_download")
 ]
--- blog/blog/dojo/views.py
+++ blog/blog/dojo/views.py
@@ -1,13 +1,46 @@
 # dojo/views.py
 
-from django.http import HttpResponse
+import os
+from django.conf import settings
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
+
 
 def mysum(request, numbers):
     # request : HttpRequest
     # numbers = "1/2/12/13/112312/13123/...."
     result = sum(map(lambda s: int(s or 0), numbers.split("/")))
+    print(numbers)
     return HttpResponse(result)
+
 
 def hello(request, name, age):
     return HttpResponse('안녕하세요. {}. {}살이시네요'.format(name, age))
+
+
+def post_list1(request):
+    name = '공유'
+    return HttpResponse('''
+
+    '''.format(name=name))
+
+
+def post_list2(request):
+    name = '공유'
+    return render(request, 'dojo/post_list.html', {'name': name})
+
+
+def post_list3(request):
+    return JsonResponse({
+        'message': '안녕, 파이썬&장고',
+        'items': ['파이썬', '장고', 'Celery', 'Azure', 'AWS'],
+    }, json_dumps_params={'ensure_ascii': False})
+
+def excel_download(request):
+    filepath = '/Users/fullyalive/Project/project_blog/blog/examplefile.xlsx'
+    # filepath = os.path.join(settings.BASE_DIR, 'examplefile.xlsx')
+    filename = os.path.basename(filepath)
+    with open(filepath, 'rb') as f:
+        response = HttpResponse(f, content_type='application/vnd.ms-excel') 
+        response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
+        return response
